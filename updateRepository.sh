f7423d7b996c3c98aeeb6c475b02215ba23fb397
Marcos Alonso <ma_lonso94@hotmail.com>
2023-01-23 12:41:22 +0100

    .

---
+++ updateRepository.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cp -r /home/zbr/.config/bspwm /home/zbr/.config/kitty /home/zbr/.config/lsd /home/zbr/.config/picom /home/zbr/.config/polybar /home/zbr/.config/rofi /home/zbr/.config/sxhkd .
+cp /home/zbr/.zshrc .
+cp /home/zbr/.p10k.zsh ./p10k.zsh
+cp /root/.p10k.zsh ./p10k.zsh.root
+
