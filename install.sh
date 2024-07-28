fb9f7d113249ec71a13bfd78f380316f30ab0dd5
zbr <zbr@kali>
2024-07-28 17:14:40 +0200

    update Dots

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PATH=/home/$1/.config
-
-cp -r ./bspwm ./kitty ./lsd ./picom ./polybar ./rofi ./sxhkd $PATH
-cp ./zshrc /home/$1/.zshrc
-cp ./p10k.zsh /home/$1/.p10k.zsh
+cp -r ./bspwm ./kitty ./lsd ./picom ./polybar ./rofi ./sxhkd /home/zbr/.config
+cp ./zshrc /home/zbr/.zshrc
+cp ./p10k.zsh /home/zbr/.p10k.zsh
 cp ./p10k.zsh.root /root/.p10k.zsh
-cp ./fonts/fonts /usr/local/share/fonts
+cp -r ./fonts/fonts /usr/local/share/fonts
